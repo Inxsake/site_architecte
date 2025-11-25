a8a078599c84b1995898ffebe0cf9f2aa4a1708e
Inxsake <baptiste.tholliez@gmail.com>
2025-11-25 11:34:30 +0100

    1130_25_11_2025_button_available

--- slideshow.js
+++ slideshow.js
@@ -1,7 +1,26 @@
 var slides = document.getElementsByClassName("slide"); // Permet de récupérer les 4 conteneurs de classe "slide"
+var prevSlide = document.getElementById("prevSlide")
+var nextSlide = document.getElementById("nextSlide")
+
+let index = 0;
+
+nextSlide.addEventListener('click', function () {
+    slides[index].style.display = "none";
+    index++
+    index=index % 4
+    showSlide(index)
+})
+
+prevSlide.addEventListener('click', function () {
+    slides[index].style.display = "none";
+    index--
+    if (index<0) {
+        index = 3
+    }
+    showSlide(index)
+})
 
 function showSlide(index){  // fonction permetant d'afficher les images
     slides[index].style.display = "block";
 }
-
-showSlide(0);
+showSlide(index)
