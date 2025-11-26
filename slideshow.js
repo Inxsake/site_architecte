71d200baf997ae292ad0e5b665b2da083fef11c8
Inxsake <baptiste.tholliez@gmail.com>
2025-11-26 10:48:55 +0100

    suppression onclick

--- slideshow.js
+++ slideshow.js
@@ -4,17 +4,17 @@ var nextSlide = document.getElementById("nextSlide")
 
 let index = 0;
 
-nextSlide.addEventListener('click', function () {
+nextSlide.addEventListener('click', function () { // Permet de passer à l'image suivante
     slides[index].style.display = "none";
     index++
-    index=index % 4
+    index=index % 4 // permet de revenir au début si l'index dépasse 3
     showSlide(index)
 })
 
-prevSlide.addEventListener('click', function () {
+prevSlide.addEventListener('click', function () { // // Permet de passer à l'image précédente
     slides[index].style.display = "none";
     index--
-    if (index<0) {
+    if (index<0) { // permet de revenir à la fin si je passe à un nombre négatif
         index = 3
     }
     showSlide(index)
